6f801c86d704983046ecf1e66ba036da7ca278b7
GergoMuller <bejla94@gmail.com>
2018-05-31 09:59:49 +0200

    Revert "initial commit"
    This reverts commit 32211fe2759aaec6fb218e69c73967f6e309455f.
    
    sh: quit: command not found

--- LoggingDAL/src/main/java/log/LogRepo.java
+++
@@ -1,9 +0,0 @@
-package log;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LogRepo extends CrudRepository<LogMessage, Long> {
-
-}
